6d0185173f7ac54dd1c8d2396c2f6e2631ee7ccc
Wesley <wesley.contato@hotmail.com>
2020-10-06 20:48:32 -0300

    Revisitando modificadores de acesso

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteContas.java
@@ -1,5 +1,6 @@
 package br.com.bytebank.banco.teste;
 
+import br.com.bytebank.banco.especial.ContaEspecial;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
@@ -7,7 +8,7 @@ import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
 public class TesteContas {
 
 	public static void main(String[] args) throws SaldoInsuficienteException{
-							
+				
 		ContaCorrente cc = new ContaCorrente(111, 111);
 		cc.deposita(100.0);
 		
