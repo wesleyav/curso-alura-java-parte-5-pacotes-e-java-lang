1ccc5c85f6756c43a42e3e33d1a2e68b83c79af6
Wesley <wesley.contato@hotmail.com>
2020-10-07 08:11:15 -0300

    String e a imutabilidade

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,17 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String nome = "Alura"; // objetc literal
+
+//		String outra = nome.replace("A", "a");
+		
+		String outra = nome.toLowerCase();
+		
+		System.out.println(nome);
+		System.out.println(outra);
+	}
+
+}
